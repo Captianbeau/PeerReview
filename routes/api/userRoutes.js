66ff372948092522d8366d309617dc47193936fd
Capti <captianbeau@gmail.com>
2024-03-23 15:58:13 -0600

    added get routes to userRoutes and created data and seed files

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getOneUser
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers);
+
+router.route('/:_id')
+.get(getOneUser);
