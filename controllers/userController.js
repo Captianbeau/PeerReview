4d2c1a00d9847068751d7f853bd29e16d92e88d8
Capti <captianbeau@gmail.com>
2024-03-23 15:38:59 -0600

    started user conntroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,22 @@
 const {Thoughts, Users} = require('../models');
+
+module.exports = {
+
+    async getUsers(req,res){
+        
+    },
+
+    async getOneUser(req,res){
+
+    },
+
+    async createUser(req,res){
+
+    },
+    async deleteUser(req,res){
+
+    },
+
+
+
+}
