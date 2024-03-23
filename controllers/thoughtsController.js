5d9e6a25fa9bf5fae2822a06e6d1dea7227153f7
Capti <captianbeau@gmail.com>
2024-03-23 15:52:40 -0600

    added get code to thoughts controller for all and single thoughts

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -3,11 +3,25 @@ const {Thoughts, Users} = require('../models');
 module.exports = {
 
     async getThoughts(req,res){
-
+        try{
+            const thoughts = await Thoughts.find().populate('thoughts');
+            res.json(thoughts)
+        }catch(err){
+            res.status(500).json(err);
+        }
     },
 
     async getOneThought(req,res){
+        try{
+            const thought = await Users.findOne({_id: req.params._id});
 
+            if(!thought){
+                return res.status(404).json({ message:'Thought not found.'});
+            }
+            res.json(thought);
+        }catch(err){
+            res.status(500).json(err);
+        }
     },
 
     async createThought(req,res){
@@ -19,7 +33,7 @@ module.exports = {
     },
 
     async updateThought(req,res){
-        
+
     }
 
 }
