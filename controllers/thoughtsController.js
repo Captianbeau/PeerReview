8fd7519fa9d6a405d4628d18e84bfe1f6521363c
Capti <captianbeau@gmail.com>
2024-03-23 15:36:21 -0600

    exported models from index and started thoughts controller

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,1 +1,25 @@
 const {Thoughts, Users} = require('../models');
+
+module.exports = {
+
+    async getThoughts(req,res){
+
+    },
+
+    async getOneThought(req,res){
+
+    },
+
+    async createThought(req,res){
+
+    },
+
+    async deleteThought(req,res){
+
+    },
+
+    async updateThought(req,res){
+        
+    }
+
+}
