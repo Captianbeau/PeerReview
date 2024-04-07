fe49a4c8bc0dbdd60a4e89ee4c16d6f2badbecc9
Capti <captianbeau@gmail.com>
2024-04-06 20:15:42 -0600

    added comments to seeds.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,27 +1,34 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { getUser, userData, thoughtData } = require('./data');
-//get data from data.js through require
 
+//Data call
+const { getUser, userData, thoughtData } = require('./data');
+
+//connect error
 connection.on('error', (err) => err);
 
+//connect 
 connection.once('open', async () => {
     console.log('seed start')
 
+    //delete if collection exists
     let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
     if (userCheck.length) {
         await connection.dropCollection('users');
     }
-
     let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
     if (thoughtCheck.length) {
         await connection.dropCollection('thoughts');
     }
+
+    //compiles the data in data.js
     getUser()
 
+    //creates collections
     const users = await User.insertMany(userData);
     const thoughts = await Thought.insertMany(thoughtData);
 
+    //gives random user ids to thoughts and saves the thought ids to user
     for (newThought of thoughts){
         const tempUser = users[Math.floor(Math.random()* users.length)]
         tempUser.thoughts.push(newThought._id);
@@ -29,16 +36,10 @@ connection.once('open', async () => {
         newThought.user.push(tempUser._id);
         await newThought.save();
 
-        console.log(tempUser._id)
     } 
 
-
-
-
-
-     
-    
-
+//tables collections in the console
+    console.table(thoughts)
     console.table(users)
     console.info('Seeding Finished');
     process.exit(0);
