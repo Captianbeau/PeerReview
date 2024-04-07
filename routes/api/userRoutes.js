0f7b906a4c03944297e3bf074d60790dd0dfe578
Capti <captianbeau@gmail.com>
2024-04-06 20:10:50 -0600

    added comments to userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,15 +10,19 @@ const {
     deleteFriend
 } = require('../../controllers/userController.js');
 
+// get and post
 router.route('/').get(getUsers).post(createUser);
 
+//get by id, delete, and put
 router.route('/:_id')
-.get(getOneUser)
-.delete(deleteUser)
-.put(updateUser);
+    .get(getOneUser)
+    .delete(deleteUser)
+    .put(updateUser);
 
+//post friend
 router.route('/:_id/friends').post(addFriend);
 
+//delete friend
 router.route('/:_id/friends/:friendId').delete(deleteFriend)
 
 module.exports = router;
